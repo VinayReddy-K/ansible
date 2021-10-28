79fb1499db1e09b2ca9705cad365f25dfda3c3ee
Vinay Reddy <89741616+VinayReddy-K@users.noreply.github.com>
2021-10-28 08:52:44 +0530

    infra

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -27,9 +27,9 @@ output "attributes" {
   value = aws_instance.ansible.*.private_ip
 }
 
-resource "local_file" "ansible" {
-  content = "[FRONTEND]\n${aws_instance.ansible.*.private_ip[9]}\n[PAYMENT]\n${aws_instance.ansible.*.private_ip[8]}\n[SHIPPING]\n${aws_instance.ansible.*.private_ip[7]}\n[USER]\n${aws_instance.ansible.*.private_ip[6]}\n[CATALOGUE]\n${aws_instance.ansible.*.private_ip[5]}\n[CART]\n${aws_instance.ansible.*.private_ip[4]}\n[REDIS]\n${aws_instance.ansible.*.private_ip[3]}\n[RABBITMQ]\n${aws_instance.ansible.*.private_ip[2]}\n[MONGODB]\n${aws_instance.ansible.*.private_ip[1]}\n[MYSQL]\n${aws_instance.ansible.*.private_ip[0]}"
-  filename = "/tmp/inv-roboshop"
-}
-
- 
+##resource "local_file" "ansible" {
+##  content = "[FRONTEND]\n${aws_instance.ansible.*.private_ip[9]}\n[PAYMENT]\n${aws_instance.ansible.*.private_ip[8]}\n[SHIPPING]\n${aws_instance.ansible.*.private_ip[7]}\n[USER]\n${aws_instance.ansible.*.private_ip[6]}\n[CATALOGUE]\n${aws_instance.ansible.*.private_ip[5]}\n[CART]\n${aws_instance.ansible.*.private_ip[4]}\n[REDIS]\n${aws_instance.ansible.*.private_ip[3]}\n[RABBITMQ]\n${aws_instance.ansible.*.private_ip[2]}\n[MONGODB]\n${aws_instance.ansible.*.private_ip[1]}\n[MYSQL]\n${aws_instance.ansible.*.private_ip[0]}"
+##  filename = "/tmp/inv-roboshop"
+##}
+##
+## 
