3e5da6531b6b656694dbe3680f466de77f66e3ca
Vinay Reddy <89741616+VinayReddy-K@users.noreply.github.com>
2021-11-04 23:05:49 +0530

    changing terraform code for app components monitor

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,33 +1,78 @@
-resource "aws_instance" "ansible" {
-  count                     = local.LENGTH
+#resource "aws_instance" "ansible" {
+#  count                     = local.LENGTH
+#  ami                       = "ami-0e4e4b2f188e91845"
+#  instance_type             = "t2.micro"
+#  vpc_security_group_ids    = ["sg-0ce90bc6ff22f0a2c"]
+#  tags                      = {
+#    Name                    = "${element(var.COMPONENTS, count.index)}-${var.ENV}"
+#  }
+#}
+
+resource "aws_instance" "app-instances" {
+  count                     = length(var.APP_COMPONENTS)
   ami                       = "ami-0e4e4b2f188e91845"
   instance_type             = "t2.micro"
   vpc_security_group_ids    = ["sg-0ce90bc6ff22f0a2c"]
   tags                      = {
-    Name                    = "${element(var.COMPONENTS, count.index)}-${var.ENV}"
+    Name                    = "${element(var.APP_COMPONENTS, count.index)}-${var.ENV}"
+    Monitor                 = "yes"
   }
 }
 
+resource "aws_instance" "db-instances" {
+  count                     = length(var.DB_COMPONENTS)
+  ami                       = "ami-0e4e4b2f188e91845"
+  instance_type             = "t2.micro"
+  vpc_security_group_ids    = ["sg-0ce90bc6ff22f0a2c"]
+  tags                      = {
+    Name                    = "${element(var.DB_COMPONENTS, count.index)}-${var.ENV}"
+  }
+}
+
+#resource "aws_route53_record" "records" {
+#  count                     = local.LENGTH
+#  name                      = "${element(var.COMPONENTS, count.index)}-${var.ENV}"
+#  type                      = "A"
+#  zone_id                   = "Z05588222UV008VFLU0F0"
+#  ttl                       = 300
+#  records                   = [element(aws_instance.ansible.*.private_ip, count.index)]
+#}
+
+
 resource "aws_route53_record" "records" {
-  count                     = local.LENGTH
-  name                      = "${element(var.COMPONENTS, count.index)}-${var.ENV}"
+  count                     = length(var.APP_COMPONENTS)
+  name                      = "${element(var.APP_COMPONENTS, count.index)}-${var.ENV}"
   type                      = "A"
   zone_id                   = "Z05588222UV008VFLU0F0"
   ttl                       = 300
-  records                   = [element(aws_instance.ansible.*.private_ip, count.index)]
+  records                   = [element(aws_instance.app-instances.*.private_ip, count.index)]
 }
 
+resource "aws_route53_record" "records" {
+  count                     = length(var.DB_COMPONENTS)
+  name                      = "${element(var.DB_COMPONENTS, count.index)}-${var.ENV}"
+  type                      = "A"
+  zone_id                   = "Z05588222UV008VFLU0F0"
+  ttl                       = 300
+  records                   = [element(aws_instance.db-instances.*.private_ip, count.index)]
+}
+
+
+#locals {
+#  LENGTH    = length(var.COMPONENTS)
+#}
+
 locals {
-  LENGTH    = length(var.COMPONENTS)
+  COMPONENT = concat(aws_instance.db-instances.*.private_ip,aws_instance.app-instances.*.private_ip)
 }
 
 ##COMPONENTS = ["mysql", "mongodb", "rabbitmq", "redis", "cart", "catalogue", "user", "shipping", "payment", "frontend"]
-output "attributes" {
-  value = aws_instance.ansible.*.private_ip
-}
+#output "attributes" {
+#  value = aws_instance.ansible.*.private_ip
+#}
 
 resource "local_file" "ansible" {
-  content         = "[FRONTEND]\n${aws_instance.ansible.*.private_ip[9]}\n[PAYMENT]\n${aws_instance.ansible .*.private_ip[8]}\n[SHIPPING]\n${aws_instance.ansible.*.private_ip[7]}\n[USER]\n${aws_instance   .ansible.*.private_ip[6]}\n[CATALOGUE]\n${aws_instance.ansible.*.private_ip[5]}\n[CART]\n${aws_instance.ansible.*.private_ip[4]}\n[REDIS]\n${aws_instance.ansible.*.private_ip[3]}\n[RABBITMQ]\n${aws_instance.ansible.*.private_ip[2]}\n[MONGODB]\n${aws_instance.ansible.*.private_ip[1]}\n[MYSQL]\n${aws_instance.ansible.*.private_ip[0]}"
+  content         = "[FRONTEND]\n${local.COMPONENT[9]}\n[PAYMENT]\n${local.COMPONENT[8]}\n[SHIPPING]\n${local.COMPONENT[7]}\n[USER]\n${local.COMPONENT[6]}\n[CATALOGUE]\n${local.COMPONENT[5]}\n[CART]\n${local.COMPONENT[4]}\n[REDIS]\n${local.COMPONENT[3]}\n[RABBITMQ]\n${local.COMPONENT[2]}\n[MONGODB]\n${local.COMPONENT[1]}\n[MYSQL]\n${local.COMPONENT[0]}"
   filename        = "/tmp/inv-roboshop-${var.ENV}"
 }
 
